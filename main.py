d017545e5756b20d098b13ec99e6a4fd1f87548d
Samchuk Maxim <samchuk.maks@outlook.com>
2024-12-08 17:44:14 +0200

    init commit

---
+++ main.py
@@ -0,0 +1,63 @@
+
+# Comand to commit code: cntl + /
+
+# var1 = 23
+# var2 = "Hello"
+# var3 = 23.333334
+# var4 = True
+# var5 = {23,"123",231.32}
+#
+# print("Var1",type(var1))
+# print("Var2",type(var2))
+# print("Var3",type(var3))
+# print("Var4",type(var4))
+# print("Var5",type(var5))
+
+
+
+# Set = {"qwerty",123,0.543,True}
+# Set.add(54)
+# print(Set)
+
+# myList=[]
+#
+# myList.append("I")
+# myList.append("love")
+# myList.append("programing")
+# myList.append("on")
+# myList.append("Python")
+# myList.append(4)
+#
+# print(myList)
+#
+# myList.remove("Python")
+# # myList.append("C++")
+# myList.insert(4,"C++")
+#
+# print(myList)
+#
+#
+# ggWp= []
+# ggWp.append("Qwerty")
+# ggWp.append("LOL")
+# ggWp.append("Roblox")
+#
+# print(ggWp)
+#
+# ggWp += myList
+# # Еквівалентні
+# # ggWp= ggWp + myList
+#
+# print(ggWp)
+#
+# print(ggWp)
+
+list=[2,12,3,1,4,5,6,7,2,45,7,-3,3,32,43.2,-0.00000020]
+print(list)
+
+list.sort()
+print(list)
+
+print(sum(list))
+print(max(list))
+print(min(list))
